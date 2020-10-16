8ec8e0d809cb64e396f7d4cd355d030c9e3376df
Octavio Cuatrochio <octaviocuatrochio45545@gmail.com>
2020-10-16 16:28:24 -0300

    añadidas mas operaciones

--- js/math.js
+++ js/math.js
@@ -87,41 +87,55 @@ function resta() {
 
 function division() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let sumado = document.getElementById("division").innerHTML;
-	document.getElementById('resultado').innerHTML = actual + sumado
+	document.getElementById('resultado').innerHTML = actual + "/"
 }
 
 function multiplicacion() {
 	let actual = document.getElementById('resultado').innerHTML;
 	let sumado = document.getElementById("multiplicacion").innerHTML;
-	document.getElementById('resultado').innerHTML = actual +  sumado
+	document.getElementById('resultado').innerHTML = actual + sumado
 }
 
-function showResult() {
+function addPunto() {
 	let actual = document.getElementById('resultado').innerHTML;
-	let suma = actual.indexOf("+");
-	let resta = actual.indexOf("-");
-	let div = actual.indexOf("รท");
-	let mult = actual.indexOf("x");
-	if (suma !== -1) {
-		arr = actual.split("+",2);
-		res = parseInt(arr[0]) + parseInt(arr[1]);
-		document.getElementById("resultado").innerHTML = res;
-	} else if (resta !== -1) {
-		arr = actual.split("-",2);
-		res = arr[0] - arr[1];
-		document.getElementById("resultado").innerHTML = res;
-		
-	} else if (div !== -1) {
-		arr = actual.split("รท",2);
-		res = arr[0] / arr[1];
-		document.getElementById("resultado").innerHTML = res;
-		
-	} else if (mult !== -1) {
-		arr = actual.split("x",2);
-		res = arr[0] * arr[1];
-		document.getElementById("resultado").innerHTML = res;
-		
-	}
-
+	document.getElementById('resultado').innerHTML = actual + "."
 }
+
+function parentesis() {
+	let actual = document.getElementById('resultado').innerHTML;
+	document.getElementById('resultado').innerHTML = "(" + actual + ")"
+}
+
+function cuadrado() {
+	let actual = document.getElementById('resultado').innerHTML;
+	document.getElementById('resultado').innerHTML = actual + ""
+}
+
+function cerrarParentesis() {
+	let actual = document.getElementById('resultado').innerHTML;
+	document.getElementById('resultado').innerHTML = actual + ")"
+}
+
+function multiplicacion() {
+	let actual = document.getElementById('resultado').innerHTML;
+	document.getElementById('resultado').innerHTML = actual + "*"
+}
+
+function inverso() {
+	let actual = document.getElementById('resultado').innerHTML;
+	document.getElementById('resultado').innerHTML = "1/" + actual
+}
+
+function factorial() {
+	let actual = document.getElementById('resultado').innerHTML;
+	document.getElementById('resultado').innerHTML = actual + "!"
+}
+
+function cuadrado() {
+	let actual = document.getElementById('resultado').innerHTML;
+	document.getElementById('resultado').innerHTML = actual + "²"
+}
+
+function raiz() {
+	let actual = document.getElementById('resultado').innerHTML;
+	document.getElementById('resultado').innerHTML = "√" + actual;
