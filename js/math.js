06795e07c112a8298a1043023b7bde4fe532044a
Florencia Ayala <flor.ayala30@gmail.com>
2020-10-16 16:09:34 -0300

    se agregaron mas funciones al js de la calculadora

--- js/math.js
+++ js/math.js
@@ -1,18 +1,27 @@
-document.getElementById("n1").addEventListener("click",n1);
-document.getElementById("n2").addEventListener("click",n2);
-document.getElementById("n3").addEventListener("click",n3);
-document.getElementById("n4").addEventListener("click",n4);
-document.getElementById("n5").addEventListener("click",n5);
-document.getElementById("n6").addEventListener("click",n6);
-document.getElementById("n7").addEventListener("click",n7);
-document.getElementById("n8").addEventListener("click",n8);
-document.getElementById("n9").addEventListener("click",n9);
-document.getElementById("n0").addEventListener("click",n0);
-document.getElementById("suma").addEventListener("click",suma);
-document.getElementById("resta").addEventListener("click",resta);
-document.getElementById("division").addEventListener("click",division);
-document.getElementById("multiplicacion").addEventListener("click",multiplicacion);
-document.getElementById("igual").addEventListener("click",showResult);
+document.getElementById("n1").addEventListener("click", n1);
+document.getElementById("n2").addEventListener("click", n2);
+document.getElementById("n3").addEventListener("click", n3);
+document.getElementById("n4").addEventListener("click", n4);
+document.getElementById("n5").addEventListener("click", n5);
+document.getElementById("n6").addEventListener("click", n6);
+document.getElementById("n7").addEventListener("click", n7);
+document.getElementById("n8").addEventListener("click", n8);
+document.getElementById("n9").addEventListener("click", n9);
+document.getElementById("n0").addEventListener("click", n0);
+document.getElementById("suma").addEventListener("click", suma);
+document.getElementById("resta").addEventListener("click", resta);
+document.getElementById("division").addEventListener("click", division);
+document.getElementById("multiplicacion").addEventListener("click", multiplicacion);
+document.getElementById(".").addEventListener("click", addPunto);
+document.getElementById("raiz").addEventListener("click", raiz);
+document.getElementById("fact").addEventListener("click", factorial);
+document.getElementById("inverso").addEventListener("click", inverso);
+document.getElementById("cuadrado").addEventListener("click", cuadrado);
+document.getElementById("n").addEventListener("click", dosElevado);
+document.getElementById("parentesis").addEventListener("click", parentesis);
+document.getElementById("limpiar").addEventListener("click", limpiar);
+document.getElementById("cuadrado").addEventListener("click", cuadrado);
+document.getElementById("igual").addEventListener("click", showResult);
 
 function n1() {
 	let actual = document.getElementById('resultado').innerHTML;
@@ -64,10 +73,6 @@ function n0() {
 	let sumado = document.getElementById("n0").innerHTML;
 	document.getElementById('resultado').innerHTML = actual + sumado
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> f0c56746df06546dab42cad9977af133d4d03246
 function suma() {
 	let actual = document.getElementById('resultado').innerHTML;
 	let sumado = document.getElementById("suma").innerHTML;
@@ -84,8 +89,6 @@ function division() {
 	let actual = document.getElementById('resultado').innerHTML;
 	let sumado = document.getElementById("division").innerHTML;
 	document.getElementById('resultado').innerHTML = actual + sumado
-<<<<<<< HEAD
-=======
 }
 
 function multiplicacion() {
@@ -120,5 +123,5 @@ function showResult() {
 		document.getElementById("resultado").innerHTML = res;
 		
 	}
->>>>>>> f0c56746df06546dab42cad9977af133d4d03246
+
 }
